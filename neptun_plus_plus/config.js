88eb693d9a181664c2e9d1e66f2e6569ee824a1b
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-07-21 11:04:17 +0200

    Initial commit - Mirrored from my site, file: neptun_plus_plus/config.js

---
+++ neptun_plus_plus/config.js
@@ -0,0 +1,22 @@
+
+const neptun1_0_config={ //Base config, waiting for migration to Neptun 2.0
+	neptun_title:"neptun.net",
+	neptun_login_distinguish_label:"login_left_side",
+	neptun_neptuncode_id:"user",
+	neptun_data_name:"date",
+	neptun_code_logged_id:"upTraining_topname",
+	neptun_training_label_id:"lblTrainingName",
+	neptun_version:1.0,
+	local_storage_main:"neptun",
+	local_storage_users:"users",
+	local_storage_courses:"courses",
+}
+
+//Langs
+const lang_EN={
+	err_ls_init:"Failed to initialize local storage!",
+	err_ls_courses:"Failed to get neptun users from local storage!",
+	err_ls_users:"Failed to get neptun courses from local storage!"
+};
+
+let lang=lang_EN;
